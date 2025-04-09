8e35cb8b6b64a406e909e70e77f302fbf388ebb1
Mario Zechner <badlogicgames@gmail.com>
2025-04-09 12:52:22 +0200

    [flutter] Update to emscripten 4.0.6, fix build script

--- spine-flutter/compile-wasm.sh
+++ spine-flutter/compile-wasm.sh
@@ -12,7 +12,7 @@ em++ \
 	-Isrc/spine-cpp/include \
 	-O2 --closure 1 -fno-rtti -fno-exceptions \
 	-s STRICT=1 \
-	-s LLD_REPORT_UNDEFINED \
+	-s ERROR_ON_UNDEFINED_SYMBOLS=1 \
 	-s MODULARIZE=1 \
 	-s ALLOW_MEMORY_GROWTH=1 \
 	-s ALLOW_TABLE_GROWTH \
