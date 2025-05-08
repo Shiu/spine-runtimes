0abcaff7fe3b991717e031cc0364c5459e1a1d23
Davide Tantillo <iamdjj@gmail.com>
2025-05-08 16:25:34 +0200

    General refactor. spine-widget to spine-skeleton.

--- spine-ts/spine-widget/src/index.ts
+++ spine-ts/spine-widget/src/index.ts
@@ -1,3 +1,4 @@
-export * from './SpineWebComponentWidget.js';
+export * from './SpineWebComponentSkeleton.js';
+export * from './SpineWebComponentOverlay.js';
 export * from "@esotericsoftware/spine-core";
 export * from "@esotericsoftware/spine-webgl";
